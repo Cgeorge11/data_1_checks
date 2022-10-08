9d5ac57266dc2ac2f686c3f8582e6afddeab67ff
Cylin George <g.cylin1@gmail.com>
2022-10-08 09:37:34 -0400

    trying datafram to format

--- Knowledge_Check_1.py
+++ Knowledge_Check_1.py
@@ -1,11 +1,20 @@
 # Import library for HTTP requests
 import requests
+#Import pandas to 
+import pandas as pd
 
 # set variable for where data will be requested from
 
 #bored_request = requests.get("https://www.boredapi.com/api/activity")
 
 bored_request = requests.get("https://datausa.io/api/data?drilldowns=Nation&measures=Population")
-
+x =bored_request.json()
 # print the results from the API call in TXT. This is to check that the HTTP call was successfull 
-print(bored_request.text)
+#print(bored_request.text)
+
+f = pd.DataFrame(x['data'])
+print(f)
+#Create data frame
+ #df = pd.DataFrame(bored_request)
+
+#print(df)
